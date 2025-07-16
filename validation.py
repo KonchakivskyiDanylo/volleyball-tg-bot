39a1db56da81c64a129f2f44b638a838edde2e2e
KonchakivskyiDanylo <konchakivskyidanylo@gmail.com>
2025-07-16 22:31:47 +0300

    Extend authorization logic to include excluded IDs; update `is_authorized` to handle string comparison and add `is_excluded_from_stats` function

--- validation.py
+++ validation.py
@@ -3,8 +3,13 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-ADMIN_IDS = os.getenv("CARD_NUMBER")
+ADMIN_IDS = os.getenv("ADMIN_IDS", "").split(",")
+EXCLUDED_IDS = os.getenv("EXCLUDED_IDS", "").split(",")
 
 
 def is_authorized(user_id):
-    return user_id in ADMIN_IDS
+    return str(user_id) in ADMIN_IDS
+
+
+def is_excluded_from_stats(user_id):
+    return str(user_id) in EXCLUDED_IDS
